726f7ad114f2302434645e12a70366ba6bf50109
อิฟฟาฮาน สุขสุวรรณ <jackcopsluncher@gmail.com>
2024-09-03 13:50:35 +0700

    optimize conftest v2

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,27 +1,20 @@
+# conftest.py
+
 import asyncio
-from contextlib import asynccontextmanager
-
 from fastapi import FastAPI
-from fastapi.testclient import TestClient
 from httpx import AsyncClient, ASGITransport
-
-
-from typing import Any, Dict, Optional
+from typing import AsyncGenerator
 from pydantic_settings import SettingsConfigDict
-
 from rubhew import models, config, main, security
 import pytest
 import pytest_asyncio
-
 import pathlib
 import datetime
-
 
 SettingsTesting = config.Settings
 SettingsTesting.model_config = SettingsConfigDict(
     env_file=".testing.env", validate_assignment=True, extra="allow"
 )
-
 
 @pytest.fixture(name="app", scope="session")
 def app_fixture():
@@ -31,23 +24,15 @@ def app_fixture():
         path.mkdir()
 
     app = main.create_app(settings)
-
     asyncio.run(models.recreate_table())
-
     yield app
-
 
 @pytest.fixture(name="client", scope="session")
 def client_fixture(app: FastAPI) -> AsyncClient:
-
-    # client = TestClient(app)
-    # yield client
-    # app.dependency_overrides.clear()
     return AsyncClient(transport=ASGITransport(app=app), base_url="http://localhost")
 
-
-@pytest_asyncio.fixture(name="session", scope="session")
-async def get_session() -> models.AsyncIterator[models.AsyncSession]:
+@pytest_asyncio.fixture(name="session", scope="function")
+async def get_session() -> AsyncGenerator[models.AsyncSession, None]:
     settings = SettingsTesting()
     models.init_db(settings)
 
@@ -57,16 +42,14 @@ async def get_session() -> models.AsyncIterator[models.AsyncSession]:
     async with async_session() as session:
         yield session
 
-
 @pytest_asyncio.fixture(name="user1")
 async def example_user1(session: models.AsyncSession) -> models.DBUser:
     password = "123456"
     username = "user1"
 
-    query = await session.exec(
+    user = await session.exec(
         models.select(models.DBUser).where(models.DBUser.username == username).limit(1)
     )
-    user = query.one_or_none()
     if user:
         return user
 
@@ -85,27 +68,25 @@ async def example_user1(session: models.AsyncSession) -> models.DBUser:
     await session.refresh(user)
     return user
 
-
 @pytest_asyncio.fixture(name="token_user1")
 async def oauth_token_user1(user1: models.DBUser) -> dict:
     settings = SettingsTesting()
     access_token_expires = datetime.timedelta(
         minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
     )
-    user = user1
     return models.Token(
         access_token=security.create_access_token(
-            data={"sub": user.id},
+            data={"sub": user1.id},
             expires_delta=access_token_expires,
         ),
         refresh_token=security.create_refresh_token(
-            data={"sub": user.id},
+            data={"sub": user1.id},
             expires_delta=access_token_expires,
         ),
         token_type="Bearer",
         scope="",
         expires_in=settings.ACCESS_TOKEN_EXPIRE_MINUTES,
         expires_at=datetime.datetime.now() + access_token_expires,
-        issued_at=user.last_login_date,
-        user_id=user.id,
+        issued_at=user1.last_login_date,
+        user_id=user1.id,
     )
